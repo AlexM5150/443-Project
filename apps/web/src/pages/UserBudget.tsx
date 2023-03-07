af4b9735bfc8aa7b497d5e299bfed14102f52ca1
David <david.aphing-kouassi@epitech.eu>
2023-03-07 00:39:53 -0800

    [ADD][web][features/navbar] responsive navbar

--- apps/web/src/pages/UserBudget.tsx
+++ apps/web/src/pages/UserBudget.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import NavBar from '../components/NavBar';
 
 const budget = {
     _id: "6406b020c9301e0eb3bfef94",
@@ -172,53 +173,55 @@ function BudgetPage() {
     }, 0);
 
     return (
-        <div className="flex justify-center items-center min-h-screen">
-              <div className="container mx-auto mt-4 border border-gray-400 rounded-md overflow-hidden sm:w-1/2 lg:w-3/4">
-                <h1 className="text-2xl font-bold bg-gray-100 p-4 mb-4">{budget._title}</h1>
-                <h2 className="text-lg font-medium p-4 mb-2">Total Budget: ${budget._budget}</h2>
-                <ul className="list-inside">
-                    {budget.expenses.map((category, index) => {
-                        const categoryClasses = ["p-4", "flex", "justify-between", index % budget.expenses.length === 0 ? "bg-red-200" : index % budget.expenses.length === 1 ? "bg-green-200" : index % budget.expenses.length === 2 ? "bg-yellow-200" : index % budget.expenses.length === 3 ? "bg-orange-200" : index % budget.expenses.length === 4 ? "bg-purple-200" : index % budget.expenses.length === 5 ? "bg-blue-200" : "bg-pink-200", "relative",];
+        <div>
+            <NavBar />
+            <div className="flex justify-center items-center mt-5">
+                <div className="container mx-auto mt-4 border border-gray-400 rounded-md overflow-hidden sm:w-1/2 lg:w-3/4">
+                    <h1 className="text-2xl font-bold bg-gray-100 p-4 mb-4">{budget._title}</h1>
+                    <h2 className="text-lg font-medium p-4 mb-2">Total Budget: ${budget._budget}</h2>
+                    <ul className="list-inside">
+                        {budget.expenses.map((category, index) => {
+                            const categoryClasses = ["p-4", "flex", "justify-between", index % budget.expenses.length === 0 ? "bg-red-200" : index % budget.expenses.length === 1 ? "bg-green-200" : index % budget.expenses.length === 2 ? "bg-yellow-200" : index % budget.expenses.length === 3 ? "bg-orange-200" : index % budget.expenses.length === 4 ? "bg-purple-200" : index % budget.expenses.length === 5 ? "bg-blue-200" : "bg-pink-200", "relative",];
 
-                        const showExpenses = categoryStates[category.category];
+                            const showExpenses = categoryStates[category.category];
 
-                        return (
-                            <li key={category.category} className={categoryClasses.join(" ")}>
-                                <button
-                                    className="text-lg font-medium mb-2"
-                                    onClick={() => toggleCategory(category.category)}
-                                >
-                                    {category.category}
-                                </button>
-                                {showExpenses && (
-                                    <ul className="list-inside ml-4">
-                                        {category.expenses.map((expense) => (
-                                            <li
-                                                key={expense._id}
-                                                className="flex justify-between mb-2"
-                                            >
-                                                <h2 className="text-lg font-medium">
-                                                    {expense.title} ${expense.cost.toFixed(2)}
-                                                </h2>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                                <h3 className="text-lg font-medium mb-2 ml-auto">
-                                    ${category.budget}
-                                </h3>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <div className="bg-gray-100 p-4">
-                    <h2 className="text-lg font-medium mb-2">
-                        Total Expenses: ${totalExpenses.toFixed(2)}
-                    </h2>
+                            return (
+                                <li key={category.category} className={categoryClasses.join(" ")}>
+                                    <button
+                                        className="text-lg font-medium mb-2"
+                                        onClick={() => toggleCategory(category.category)}
+                                    >
+                                        {category.category}
+                                    </button>
+                                    {showExpenses && (
+                                        <ul className="list-inside ml-4">
+                                            {category.expenses.map((expense) => (
+                                                <li
+                                                    key={expense._id}
+                                                    className="flex justify-between mb-2"
+                                                >
+                                                    <h2 className="text-lg font-medium">
+                                                        {expense.title} ${expense.cost.toFixed(2)}
+                                                    </h2>
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    )}
+                                    <h3 className="text-lg font-medium mb-2 ml-auto">
+                                        ${category.budget}
+                                    </h3>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                    <div className="bg-gray-100 p-4">
+                        <h2 className="text-lg font-medium mb-2">
+                            Total Expenses: ${totalExpenses.toFixed(2)}
+                        </h2>
+                    </div>
                 </div>
             </div>
         </div>
-
     );
 
 }
