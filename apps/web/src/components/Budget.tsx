20cabdeb982ae902ca78ceb059ab6a1c7de9dbaa
David <david.aphing-kouassi@epitech.eu>
2023-03-08 20:35:13 -0800

    [EDIT][web][features/user-budget-page] Budget title component

--- apps/web/src/components/Budget.tsx
+++ apps/web/src/components/Budget.tsx
@@ -1,5 +1,4 @@
 import Title from "./Title";
-import TotalBudget from "./TotalBudget";
 import Category from "./Category"
 import Expenses from "./Expenses"
 import { IBudget, ICategoryStates } from "../types"
@@ -7,10 +6,9 @@ import { IBudget, ICategoryStates } from "../types"
 function Budget({ budget, isOpen, toggleOpen, categoryStates, toggleCategory }: { budget: IBudget, isOpen: boolean, toggleOpen: () => void, categoryStates: ICategoryStates, toggleCategory: (category: string) => void }) {
     return (
         <div>
-            <Title title={budget._title} onClick={toggleOpen} />
+            <Title title={budget._title} budget={budget._budget} onClick={toggleOpen} />
             {isOpen && (
                 <>
-                    <TotalBudget budget={budget._budget} />
                     {budget.expenses.map((category, key) => {
                         const showExpenses = categoryStates[category.category];
                         return (
@@ -28,7 +26,7 @@ function Budget({ budget, isOpen, toggleOpen, categoryStates, toggleCategory }: 
                             </div>
                         );
                     })}
-                    <div className="bg-gray-100 p-4">
+                    <div className="bg-gray-200 p-4">
                         <h2 className="text-lg font-medium mb-2">Total Expenses: ${budget._current}</h2>
                     </div>
                 </>
--- apps/web/src/components/TotalBudget.tsx
+++
@@ -1,9 +0,0 @@
-function TotalBudget({ budget }: { budget: number }) {
-    return (
-        <div className="bg-gray-100">
-            <h2 className="text-lg font-medium p-4">Total Budget: ${budget}</h2>
-        </div>
-    );
-}
-
-export default TotalBudget
