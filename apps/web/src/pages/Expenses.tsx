d42b79bfbb15ba21fe20e4c67e31850b898a6c80
Dolan <57276201+dolan77@users.noreply.github.com>
2023-03-14 17:21:36 -0700

    [ADD][WEB] added more functionality to expenses. find a way to send form data to the db.

--- apps/web/src/pages/Expenses.tsx
+++ apps/web/src/pages/Expenses.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Navbar from "../components/NavBar";
+import { IError } from "../types";
+import Server from "../tools/Server";
+import { Notification } from "../components";
+// Created by Dylan Huynh and Deric Cheng
 export default function Expenses() {
-
+    const [show, setShow] = useState<IError>({ message: "", active: false });
     const [categories, setCategories] = useState<{value: string; label: string}[]>([{value:"", label:""}]);
     // get a list of categories by calling the API
     // if no categories exist, the user cannot create an expense so check for that
@@ -26,8 +30,12 @@ export default function Expenses() {
     useEffect(() => {
         // make the api call here?
         const category = [
-            {value: 'food', label: "Food"},
-            {value: 'gas', label: "Gas"},
+            {value: '00000', label: "Food"},
+            {value: '00001', label: "Gas"},
+            {value: '00002', label: "Travel"},
+            {value: '00003', label: 'Shoes'},
+            {value: '00004', label: 'Dessert'},
+            {value: '00005', label: 'Video Games'}
         ];
 
         setCategories(category)
@@ -40,39 +48,47 @@ export default function Expenses() {
      * @returns a react component
      */
     const DropDownMenu = () => {
-        const [preview, setpreview] = useState<string>('category');
-        const [showMenu, setShowMenu] = useState<boolean>(false);
-
-
+        /**
+         * render the drop down menu
+         */
         useEffect(() => {
             PopulateDropDown()
         })
 
-        const handleChange = (value: string) => {
-            setpreview(value)
-        }
-
+        /**
+         * method that populates the drop down menu for the categories
+         * @returns a React component that renders the drop down menu based off the amount of categories in the db
+         */
         const PopulateDropDown = () => {
             return(
-            <div className="text-left border boreder-gray-400" >
-                <div className="p-1 items-center justify-between select-none w-32" onClick={() => setShowMenu(!showMenu)}>
-                    {preview}
-                </div>
-                {showMenu && (<div className="border max-h-40 overflow-auto rounded">
+                <div className="px-2">
+                    <label className="text-sm flex mb-1">Category</label>
+                    
+                    <select name="category" required className="p-2 my-1.5 overflow-y-auto">
                         {categories.map(option => (
-                            <div key={option.value} className="p-1 cursor-pointer hover:bg-gray-200"
-                            onClick={() => handleChange(option.label)}> {option.label} </div>
+                            <option className="p-1 cursor-pointer" key={option.value} value={option.value}>{option.label}</option>
                         ))}
-                    </div>)}
-            </div>
+
+                    </select>
+                    
+                    
+                </div>
             )
         }
         return(
             <div>{PopulateDropDown()}</div>
         )
     }
-    const handleSubmit = () => {
-
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const {category, title, cost} = form.elements as typeof form.elements & {
+            category: HTMLInputElement;
+            title: HTMLInputElement;
+            cost: HTMLInputElement;
+        }
+        // use an API call here to create an expense.
+        // follow what Carlos did in App.tsx. also find a way to auto generate ID's
     }
 
     return(
@@ -81,41 +97,46 @@ export default function Expenses() {
             <div className="container mx-auto mt-8 border border-gray-400 rounded-md overflow-hidden sm:w-1/2 lg:w-3/4 h-5/6">
                 <div className="text-center text-2xl mb-2 mt-2 justify-center items-center content-center">Add Expense</div>
                 <div className="h-96 flex justify-evenly border border-t-gray-400">
-                    <form className="row-span-4 space-y-2 flex" onSubmit={handleSubmit}>
-                        <div>
-                            <DropDownMenu />
-                        </div>
+                    <form className="row-span-4 flex mt-4" onSubmit={handleSubmit}>
+                        
+                        <DropDownMenu/>
+                        
+                        
+                        
                         <div className="px-2">
                         <label className="text-sm flex mb-1">Name</label>
                         <input
                             autoComplete="off"
                             required
-                            id="expense_name"
+                            id="title"
                             type="text"
                             placeholder="Pizza"
                             className="w-full py-1.5 leading-loose px-2"
                         />
                         </div>
+                        {/* ensure that AMOUNT does not allow a negative number */}
                         <div className="px-2">
                             <label className="text-sm flex mb-1">Amount</label>
                             <input
                             autoComplete="off"
                             required
-                            id="amount"
+                            id="cost"
                             placeholder="4.99"
                             className="w-full py-1.5 leading-loose px-2"
                             type="number"
                             min = "0"
-                            
                             />
                             
                         </div>
-
+                        <div className="px-2">
+                        <input type="submit" value="Submit" className="border"/>
+                        </div>
+                        
 
                         
                     </form>
                 </div>
-                
+            <Notification display={[show, setShow]} />
             </div>
         </div>
 
