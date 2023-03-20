99c327d1bd8bdcd3796ec53252f50339047e8f32
Dolan <57276201+dolan77@users.noreply.github.com>
2023-03-20 12:02:52 -0700

    [ADD}[WEB]Created Notifications.tsx with initial budget data

---
+++ apps/web/src/pages/Notifications.tsx
@@ -0,0 +1,161 @@
+import React from "react";
+
+// sample budget
+const budget = {
+    _id: "6406b020c9301e0eb3bfef94",
+    _user: "6406125e32cc81699ab4c446",
+    _current: 15,
+    _budget: 100,
+    _title: "March 2023",
+    expenses: [
+        {
+            "_id": "abc",
+            "category": "Transport",
+            "budget": 200,
+            "expenses": [
+                {
+                    "title": "Rocket pass",
+                    "cost": 15,
+                    "category": "road to champion",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+        {
+            "_id": "def",
+            "category": "Food",
+            "budget": 150,
+            "expenses": [
+                {
+                    "title": "Octane blanche",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane rouge",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane bleue",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane blanche",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane rouge",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane bleue",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane blanche",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane rouge",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                },
+                {
+                    "title": "Octane bleue",
+                    "cost": 15,
+                    "category": "road to bronze",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+        {
+            "_id": "abc",
+            "category": "Joga",
+            "budget": 200,
+            "expenses": [
+                {
+                    "title": "Rocket pass",
+                    "cost": 15,
+                    "category": "road to champion",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+        {
+            "_id": "abc",
+            "category": "Handball",
+            "budget": 200,
+            "expenses": [
+                {
+                    "title": "Rocket pass",
+                    "cost": 15,
+                    "category": "road to champion",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+        {
+            "_id": "abc",
+            "category": "Football",
+            "budget": 200,
+            "expenses": [
+                {
+                    "title": "Rocket pass",
+                    "cost": 15,
+                    "category": "road to champion",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+        {
+            "_id": "abc",
+            "category": "Volley",
+            "budget": 200,
+            "expenses": [
+                {
+                    "title": "Rocket pass",
+                    "cost": 15,
+                    "category": "road to champion",
+                    "created": "3/6/2023, 7:32:18\u202fPM",
+                    "_id": "6406b043c9301e0eb3bfef97"
+                }
+            ]
+        },
+    ]
+}
+
+
+export default function Notifications(){
+
+    // create a progress bar and display the progress / amount spent on all budget categories and total budget
+    return(
+        <div>Testing</div>
+    )
+}
