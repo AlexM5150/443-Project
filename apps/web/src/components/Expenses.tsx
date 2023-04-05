3997fe347851d72600c86e988e5d42094c99d48e
Dolan <57276201+dolan77@users.noreply.github.com>
2023-04-05 16:53:33 -0700

    [add][web] added buttons that represent editing an expense and deleting an expense

--- apps/web/src/components/Expenses.tsx
+++ apps/web/src/components/Expenses.tsx
@@ -1,7 +1,17 @@
+import { AiTwotoneEdit, AiTwotoneDelete } from "react-icons/ai";
 function Expenses({ title, cost }: { title: string, cost: number }) {
     return (
         <div className="flex flex-row justify-between">
+            <div className="flex justify-between">
             <h1 className="font-medium p-2">{title} ${cost}</h1>
+            {/* deric handle these two. editing an expense and deleting an expense*/}
+            <button>
+                <AiTwotoneEdit className="w-6 h-7 "></AiTwotoneEdit>
+            </button>
+            <button>
+                <AiTwotoneDelete className="w-6 h-7"></AiTwotoneDelete>
+            </button>
+            </div>
         </div>
     );
 }
